2b6b002eeea9d27215561e16ab0bbc8ba7a19370
wdcpclover <wdcpclover@qq.com>
2025-07-01 10:35:50 +0900

    Initial commit

---
+++ .ipynb_checkpoints/1.py
@@ -0,0 +1,65 @@
+age = 20
+gender = "女"
+
+if gender == "女":
+    if age >= 18:
+        print("成年女性")
+    else:
+        print("未成年女性")
+else:
+    if age >= 18:
+        print("成年男性")
+    else:
+        print("未成年男性")
+
+        if gender == "女":
+            if age >= 18:
+                print("成年女性")
+            else:
+                print("未成年女性")
+        else:
+            if age >= 18:
+                print("成年男性")
+            else:
+                print("未成年男性")
+if gender == "女":
+    if age >= 18:
+        print("成年女性")
+    else:
+        print("未成年女性")
+else:
+    if age >= 18:
+        print("成年男性")
+    else:
+        print("未成年男性")
+if gender == "女":
+    if age >= 18:
+        print("成年女性")
+    else:
+        print("未成年女性")
+else:
+    if age >= 18:
+        print("成年男性")
+    else:
+        print("未成年男性")
+if gender == "女":
+    if age >= 18:
+        print("成年女性")
+    else:
+        print("未成年女性")
+else:
+    if age >= 18:
+        print("成年男性")
+    else:
+        print("未成年男性")
+if gender == "女":
+    if age >= 18:
+        print("成年女性")
+    else:
+        print("未成年女性")
+else:
+    if age >= 18:
+        print("成年男性")
+    else:
+        print("未成年男性")
+        print
--- 第一部分python/mymodule.py
+++ 第一部分python/mymodule.py
@@ -1,3 +1,5 @@
 # mymodule.py
 def greet(name):
-    return f"你好, {name}!"
+    return f"你好1, {name}!"
+def greet1(name):
+    return f"你好1, {name}!"
